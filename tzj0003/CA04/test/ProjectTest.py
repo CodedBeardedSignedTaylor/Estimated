574f9e9f7703e5790c778128ec755b566cc1eb5e
Taylor Jones <tzj0003@auburn.edu>
2014-11-04 15:19:14 -0600

    Red Light Tests for Project - Scenario 2
    Signed-off-by: Taylor Jones <tzj0003@auburn.edu>

--- tzj0003/CA04/test/ProjectTest.py
+++ tzj0003/CA04/test/ProjectTest.py
@@ -12,3 +12,20 @@ class Test(unittest.TestCase):
     def test100_010_shouldConstructProject(self):
         project = Project.Project()
         self.assertIsInstance(project, Project.Project)
+    
+    def test200_010_shouldAddIteration(self):
+        project = Project.Project()
+        iteration = Iteration.Iteration(effort=20, projectedVelocity=4)
+        self.assertEquals(project.addIteration(iteration), 1)
+    
+    def test200_020_addShouldRejectInvalidParameters(self):
+        expectedString = "Project.add:  invalid parameters"
+        project = Project.Project()
+        try:
+            project.add(69)                                                
+            self.fail("exception was not raised")                    
+        except ValueError as raisedException:                                           
+            diagnosticString = raisedException.args[0]                                   
+            self.assertEquals(expectedString, diagnosticString[0:len(expectedString)]) 
+        except:
+            self.fail("incorrect exception was raised")
