a28838128c72e3e2895ae101b6c43095d5d670d9
Taylor Jones <tzj0003@auburn.edu>
2014-11-06 13:14:07 -0600

    Red Light Tests for Calendar - Scenarios 1 & 2
    Signed-off-by: Taylor Jones <tzj0003@auburn.edu>

---
+++ tzj0003/CA04/prod/Calendar.py
@@ -0,0 +1,17 @@
+'''
+Created on Nov 6, 2014
+
+@author: Taylor
+'''
+
+class Calendar(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
---
+++ tzj0003/CA04/test/CalendarTest.py
@@ -0,0 +1,44 @@
+import unittest
+# import CA04.prod.Project as Project
+# import CA04.prod.Iteration as Iteration
+import CA04.prod.Calendar as Calendar
+
+class TestComponent(unittest.TestCase):
+    
+    def test100_010_shouldConstructCalendar(self):
+        cal = Calendar.Calendar()
+        self.assertIsInstance(cal, Calendar.Calendar)
+    
+    def test200_010_shouldAddDayToCalendar(self):
+        cal = Calendar.Calendar()
+        cal.add(day=1, effort=25)
+        a = cal.add(day=2, effort=30)
+        self.assertEquals(a, 55)
+    
+    def test200_020_shouldRejectInvalidDay(self):
+        expectedString = "Calendar.add:  invalid day parameter."
+        cal = Calendar.Calendar()
+        try:
+            cal.add(day="today", effort=20)
+            self.fail("exception was not raised")
+        except ValueError as raisedException:                                           
+            diagnosticString = raisedException.args[0]                                   
+            self.assertEquals(expectedString, diagnosticString[0:len(expectedString)]) 
+        except:
+            self.fail("incorrect exception was raised")
+    
+    def test200_030_shouldRejectInvalidEffort(self):
+        expectedString = "Calendar.add:  invalid effort parameter."
+        cal = Calendar.Calendar()
+        try:
+            cal.add(day=3, effort="hard")                                               
+            self.fail("exception was not raised")                    
+        except ValueError as raisedException:                                           
+            diagnosticString = raisedException.args[0]                                   
+            self.assertEquals(expectedString, diagnosticString[0:len(expectedString)]) 
+        except:
+            self.fail("incorrect exception was raised")
+            
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
