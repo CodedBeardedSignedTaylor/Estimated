15054b9d2535a2334d7ef66fa05a3cee7a2f63a9
Taylor Jones <tzj0003@auburn.edu>
2014-11-10 21:20:46 -0600

    Review 1 of Schedule.
    Signed-off-by: Taylor Jones <tzj0003@auburn.edu>

--- tzj0003/CA04/test/ScheduleTest.py
+++ tzj0003/CA04/test/ScheduleTest.py
@@ -113,7 +113,30 @@ class Test(unittest.TestCase):
         s = Schedule.Schedule(proj, cal)
         
         self.assertEquals(s.getPV(day=3), 3)
-        
+    
+    def test400_020_shouldRejectInvalidDay(self):
+        expectedString = "Schedule.getPV:  requested day not in range."
+        # Create calendar and add days.
+        cal = Calendar.Calendar()
+        cal.add(day=1, effort=10)
+        cal.add(day=2, effort=0)
+        cal.add(day=3, effort=30)
+        cal.add(day=4, effort=30)
+        cal.add(day=5, effort=60)
+        cal.add(day=6, effort=90)
+        # Create Project and add iterations.
+        proj = Project.Project()
+        proj.add(Iteration.Iteration(effort=30, plannedVelocity=1))
+        proj.add(Iteration.Iteration(effort=60, plannedVelocity=3))
+        s = Schedule.Schedule(proj, cal)
+        try:
+            s.getPV(400)                                             
+            self.fail("exception was not raised")                    
+        except ValueError as raisedException:                                           
+            diagnosticString = raisedException.args[0]                                   
+            self.assertEquals(expectedString, diagnosticString[0:len(expectedString)]) 
+        except:
+            self.fail("incorrect exception was raised")
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
